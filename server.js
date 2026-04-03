ec99ca3f0a655f3bb39b917395724993a78121cc
gamingdrip <58792718+gamingdrip@users.noreply.github.com>
2026-04-02 18:06:12 -0600

    Update server.js

--- server.js
+++ server.js
@@ -10,74 +10,108 @@ app.use(express.static(__dirname));
 
 const players = {};
 const cannonballs = [];
+let chests = [];
 
-io.on("connection", (socket) => {
-  console.log("A pirate joined! ID: " + socket.id);
+function spawnChests() {
+  chests = [];
+  for (var i = 0; i < 15; i++) {
+    chests.push({
+      id: i,
+      x: Math.random() * 2200 + 100,
+      y: Math.random() * 1600 + 100,
+      gold: Math.floor(Math.random() * 80) + 20,
+      taken: false
+    });
+  }
+}
+spawnChests();
 
-  players[socket.id] = {
-    x: 400, y: 250, angle: 0,
-    name: "Pirate", clan: "", gold: 200, health: 100
-  };
+io.on("connection", function(socket) {
+  socket.on("join", function(data) {
+    players[socket.id] = {
+      x: Math.random() * 600 + 100,
+      y: Math.random() * 300 + 100,
+      angle: 0,
+      name: data.name || "Pirate",
+      clan: "",
+      gold: 200,
+      health: 100,
+      shipColor: data.shipColor || "#8B4513"
+    };
+    socket.emit("currentPlayers", players);
+    socket.emit("chestsUpdate", chests);
+    socket.broadcast.emit("newPlayer", Object.assign({ id: socket.id }, players[socket.id]));
+  });
 
-  socket.emit("currentPlayers", players);
-  socket.broadcast.emit("newPlayer", { id: socket.id, ...players[socket.id] });
-
-  socket.on("move", (data) => {
+  socket.on("move", function(data) {
     if (players[socket.id]) {
       players[socket.id].x = data.x;
       players[socket.id].y = data.y;
       players[socket.id].angle = data.angle;
-      socket.broadcast.emit("playerMoved", { id: socket.id, ...players[socket.id] });
+      players[socket.id].gold = data.gold;
+      socket.broadcast.emit("playerMoved", Object.assign({ id: socket.id }, players[socket.id]));
     }
   });
 
-  socket.on("shoot", (data) => {
-    const ball = {
-      id: Date.now() + Math.random(),
-      ownerId: socket.id,
-      x: data.x, y: data.y,
-      angle: data.angle,
-      speed: 6
-    };
+  socket.on("collectChest", function(chestId) {
+    var chest = chests.find(function(c) { return c.id === chestId && !c.taken; });
+    if (chest && players[socket.id]) {
+      chest.taken = true;
+      players[socket.id].gold += chest.gold;
+      io.emit("chestCollected", { chestId: chestId, playerId: socket.id, gold: chest.gold });
+      setTimeout(function() {
+        chest.x = Math.random() * 2200 + 100;
+        chest.y = Math.random() * 1600 + 100;
+        chest.gold = Math.floor(Math.random() * 80) + 20;
+        chest.taken = false;
+        io.emit("chestsUpdate", chests);
+      }, 10000);
+    }
+  });
+
+  socket.on("shoot", function(data) {
+    var ball = { id: Date.now(), ownerId: socket.id, x: data.x, y: data.y, angle: data.angle, speed: 6 };
     cannonballs.push(ball);
     io.emit("cannonball", ball);
   });
 
-  socket.on("disconnect", () => {
-    console.log("A pirate left! ID: " + socket.id);
+  socket.on("disconnect", function() {
     delete players[socket.id];
     io.emit("playerLeft", socket.id);
   });
 });
 
-setInterval(() => {
-  cannonballs.forEach((ball, i) => {
+setInterval(function() {
+  var leaderboard = Object.values(players)
+    .sort(function(a, b) { return b.gold - a.gold; })
+    .slice(0, 5)
+    .map(function(p) { return { name: p.name, gold: p.gold }; });
+  io.emit("leaderboard", leaderboard);
+
+  for (var i = cannonballs.length - 1; i >= 0; i--) {
+    var ball = cannonballs[i];
     ball.x += Math.cos(ball.angle) * ball.speed;
     ball.y += Math.sin(ball.angle) * ball.speed;
-
-    Object.keys(players).forEach(id => {
+    Object.keys(players).forEach(function(id) {
       if (id === ball.ownerId) return;
-      const dx = players[id].x - ball.x;
-      const dy = players[id].y - ball.y;
-      if (Math.sqrt(dx*dx + dy*dy) < 20) {
+      var dx = players[id].x - ball.x;
+      var dy = players[id].y - ball.y;
+      if (Math.sqrt(dx * dx + dy * dy) < 20) {
         players[id].health -= 20;
-        io.emit("playerHit", { id, health: players[id].health });
+        io.emit("playerHit", { id: id, health: players[id].health });
         cannonballs.splice(i, 1);
         if (players[id].health <= 0) {
           players[id].health = 100;
           players[id].x = Math.random() * 600 + 100;
           players[id].y = Math.random() * 400 + 50;
-          io.emit("playerRespawn", { id, x: players[id].x, y: players[id].y });
+          io.emit("playerRespawn", { id: id, x: players[id].x, y: players[id].y });
         }
       }
     });
+    if (ball.x < 0 || ball.x > 2400 || ball.y < 0 || ball.y > 1800) cannonballs.splice(i, 1);
+  }
+}, 2000);
 
-    if (ball.x < 0 || ball.x > 800 || ball.y < 0 || ball.y > 500) {
-      cannonballs.splice(i, 1);
-    }
-  });
-}, 1000 / 60);
-
-server.listen(3000, () => {
+server.listen(3000, function() {
   console.log("Argh.io server running at http://localhost:3000");
 });
